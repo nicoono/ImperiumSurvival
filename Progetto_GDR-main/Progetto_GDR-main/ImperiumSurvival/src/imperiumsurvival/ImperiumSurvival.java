13a2f4187009878e846ce1d7d68bcad2ccde0f3e
nicoono <onorati.nicolo@franchettisalviani.net>
2026-03-16 09:21:42 +0100

    risoluzione vari problemi
    problemi dati da git

---
+++ Progetto_GDR-main/Progetto_GDR-main/ImperiumSurvival/src/imperiumsurvival/ImperiumSurvival.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package imperiumsurvival;
+
+/**
+ *
+ * @author onorati.nicolo
+ */
+public class ImperiumSurvival {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ScegliPersonaggio sp = new ScegliPersonaggio();
+        sp.setVisible(true);
+    }
+    
+}
